297aacd8e84f70ee45c5ecf9783cb157e0e7b323
Alon Hammerman <m0she@audish.com>
2012-03-28 12:30:08 +0200

    CachedExternalHtmlRelocation - dismantle beast
    1) Externify processor
    2) render_to_string and externified_view
    
    Also:
    - fix scss premature import

--- relocation/processors.py
+++ relocation/processors.py
@@ -2,15 +2,9 @@ import hashlib, logging
 from bunch import Bunch
 
 from django.conf import settings
-from django.core.cache import cache as default_cache
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse
-from django.template.context import RequestContext
 
 from .cache import cached_data
-from .djangoutils.utils import do_relocation
-from .jinja import env, context_to_dict
-from .engine import RelocationSerializer
 from .utils import buf_to_unicode, smart_import
 
 CACHE_NAME='relocations'
@@ -25,77 +19,55 @@ def patch_cached_response(request, response):
     if func_name:
         return smart_import(func_name)(request, response)
 
-class CachedExternalHtmlRelocation(object):
-    def __init__(self, template, cache=None):
-        self.template = template
-        self.cache = cache or default_cache
+def external_http_reference(destination_format, reverse_view):
+    return lambda template_name, section_name, section_data: (
+        destination_format % reverse(reverse_view, args=(template_name, section_data)))
 
-    EMPTY_HASH = '18abebe'
-    SECTION_RULES = Bunch(
-        javascript = Bunch(
+EXTERNIFY_VIEW = getattr(settings, 'RELOCATION_EXTERNIFY_VIEW', None)
+EXTERNIFY_SECTION_RULES = Bunch(
+    javascript = Bunch(
+        reference = external_http_reference(
             destination_format = '<script type="text/javascript" src="%s"></script>',
-            mimetype = 'application/javascript',
+            reverse_view = EXTERNIFY_VIEW,
         ),
-        css = Bunch(
+        mimetype = 'application/javascript',
+    ),
+    css = Bunch(
+        reference = external_http_reference(
             destination_format = '<link rel="stylesheet" type="text/css" href="%s"/>',
-            mimetype = 'text/css',
+            reverse_view = EXTERNIFY_VIEW,
         ),
-    )
+        mimetype = 'text/css',
+    ),
+)
 
-    def process(self, main, sections):
-        for section in self.SECTION_RULES:
-            if section in sections:
-                cached_url = self.put_cache_data(section, buf_to_unicode(sections[section]))
-                sections[section].clear()
-                sections[section].append(self.SECTION_RULES[section].destination_format % cached_url)
-        return main, sections
+def externify(template_name, main, sections, rules=EXTERNIFY_SECTION_RULES, prefix='externified_'):
+    for section_name, ruledata in rules.items:
+        if section_name not in sections:
+            continue
+        new_section_name = prefix+section_name
+        sections[new_section_name] = sections[section_name].copy()
+        sections[section_name].clear()
+        sections[section_name].append(ruledata.reference(template_name, section_name, sections[new_section_name]))
 
-    def do_relocation(self, rendered_template):
-        return buf_to_unicode(self.process(RelocationSerializer.deserialize(rendered_template))[0])
+scss_compiler = None
+def get_scss_compiler():
+    global scss_compiler
+    if scss_compiler:
+        return scss_compiler
 
-    def put_cache_data(self, section, data):
-        data_hash = hashlib.md5(data).hexdigest()
-        self.cache.set(self.get_key(section, data_hash), data)
-        # Cache latest with no HASH
-        self.cache.set(self.get_key(section, self.EMPTY_HASH), data)
-        return reverse(self.get_cached_data, kwargs=dict(template_name=self.template, section=section, data_hash=data_hash))
-
-    @classmethod
-    def get_cached_data(cls, request, template_name, section, data_hash=EMPTY_HASH):
-        self = cls(template_name)
-        response_content = self.cache.get(self.get_key(section, data_hash))
-        if response_content is None:
-            self.render(context_to_dict(RequestContext(request)))
-            response_content = self.cache.get(self.get_key(section, data_hash))
-        response = HttpResponse(response_content, mimetype=self.SECTION_RULES[section].get('mimetype', None))
-        patch_cached_response(request, response)
-
-        return response
-
-    def render(self, context):
-        # TODO: Shouldn't be here, should it?
-        return do_relocation(self.template, env.get_or_select_template(self.template).render(context))
-
-    def get_key(self, section, data_hash):
-        return 'external_relocation_cache.%s.%s.%s' % (self.template, section, data_hash)
-
-
-def cached_external(template_name, main, sections, cache=None):
-    instance = CachedExternalHtmlRelocation(template_name, cache)
-    return instance.process(main, sections)
-
-def setup_scss():
     import scss
     # Use our own logger instead of their default
     scss.log = logging.getLogger('reloc.scss')
-    return scss.Scss()
-scss_compiler = setup_scss()
+    scss_compiler = scss.Scss()
+    return scss_compiler
+
 def scss(template_name, main, sections):
     scss_sections = ('css',)
     for section in scss_sections:
         if section not in sections:
             continue
-        scssed = relocation_cache_get_or_set('scss', buf_to_unicode(sections[section]), lambda data: scss_compiler.compile(data))
+        scssed = relocation_cache_get_or_set('scss', buf_to_unicode(sections[section]), lambda data: get_scss_compiler().compile(data))
         sections[section].clear()
         sections[section].append(scssed)
 
