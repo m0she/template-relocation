9a6c51c0cf78ffa1d20a37735ee39d6f7b1426f3
Alon Hammerman <m0she@audish.com>
2012-03-29 12:35:13 +0200

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='template-relocation',
+    version=0.7,
+    author='Alon Hammerman',
+    author_email='the.alonso@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/m0she/template-relocation',
+    license='New BSD License',
+    description='Write web modules using HTML, Javascript (CoffeeScript) and CSS (Scss) all in the same file. And have them dynamically and efficiently served',
+)
